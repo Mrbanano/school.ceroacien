bebd2d7c41857fe46cd74f402a25c7b8875e81fa
AlvaroCastillo <6666alvaro666@gmail.com>
2022-08-15 12:46:05 -0500

    chec ?

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ export default function IndexPage() {
   return (
     <Layout>
       <h1>NextAuth.js Example</h1>
-      <p>{process.env.NEXTAUTH_URL}</p>
+      <p>aqui va la variable {process.env.NEXTAUTH_URL}</p>
       <p>
         This is an example site to demonstrate how to use{" "}
         <a href="https://next-auth.js.org">NextAuth.js</a> for authentication.
