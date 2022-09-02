cc83a3c6869a9219c52beb4ae99b6bb7e1cd7f47
AlvaroCastillo <6666alvaro666@gmail.com>
2022-09-01 23:48:34 -0500

    change de banner

--- components/BannerAds/Index.js
+++ components/BannerAds/Index.js
@@ -7,14 +7,14 @@ export default function Index({ show }) {
       <section className="p-2 bg-primary w-full relative">
         <div className=" max-w-7xl mx-auto text-center flex flex-col sm:flex-row justify-center items-center gap-1">
           <h6 className="text-white font-semibold text-sm md:text-base">
-            POR LANZAMIENTO:
+            DESCUBRE:
           </h6>
           <h6 className="text-white font-semibold text-xs md:text-base">
-            tome nuestro curso curso de Python 3 gratis.
+            Todo lo que tenemos para tí
           </h6>
           <Link href="/course">
             <a className="text-white font-semibold text-xs  md:text-base underline">
-              <h6>Empezar ahora </h6>
+              <h6 className="ml-2">Ver </h6>
             </a>
           </Link>
         </div>
